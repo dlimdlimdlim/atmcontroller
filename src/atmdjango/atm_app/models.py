61bb8378f8fd7e052fabe1bb3425b46d83cf939d
dlimdlim <dlim7272@gmail.com>
2021-02-23 01:26:30 +0900

    work on repo

--- src/atmdjango/atm_app/models.py
+++ src/atmdjango/atm_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from account.entity import Account, AccountRecord, Card
 # Create your models here.
 
 
@@ -8,6 +9,9 @@ class BankCard(models.Model):
     card_number = models.BigIntegerField(unique=True)
     pin_hash = models.CharField(max_length=128)
 
+    def to_domain(self):
+        return Card(user_id=self.user_id, card_num=self.card_number, pin_salt_hash=self.pin_hash)
+
 
 class BankAccount(models.Model):
     user_id = models.BigIntegerField(db_index=True)
@@ -15,6 +19,9 @@ class BankAccount(models.Model):
 
     class Meta:
         unique_together = [['user_id', 'account_name']]
+
+    def to_domain(self):
+        return Account(account_id=self.id, user_id=self.user_id, name=self.account_name, histories=[])
 
 
 class AccountHistory(models.Model):
@@ -25,4 +32,16 @@ class AccountHistory(models.Model):
 
     class Meta:
         index_together = [['account_id', 'created_at']]
+
+    def to_domain(self):
+        return AccountRecord(action=self.operation, balance=self.account_balance, time_at=self.created_at)
+
+    @staticmethod
+    def from_domain(account_record: AccountRecord, account_id: int):
+        AccountHistory.objects.create(
+            account_id=account_id,
+            operation=account_record.action,
+            account_balance=account_record.balance
+        )
+
 
